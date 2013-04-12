09db4b6a1ed1cad01a3d4a7aa9a4cfbbfe245c47
Florian Angeletti <florian.angeletti@achronie.fr>
2013-04-12 15:43:42 +0200

    -Hidden markov chain cogeneration -example file

--- tutorial/tHiddenChain.m
+++ tutorial/tHiddenChain.m
@@ -1,5 +1,5 @@
 % Desired size of the signal
-n=1000;
+n=100;
 
 % Projection matrix : Initial distribution of the hidden Markov chain
 A = [ 1 1; 1 1];
@@ -9,14 +9,18 @@ p=0.8; q= 0.2;
 E = [ p q; q p];
 
 % Distribution matrix: Matrix of the laws of the observable
-sigma=1;
+sigma=0.75;
+% lNormal(mu,sigma) : gaussian random variable with average mu and variance sigma^2 
 Laws={ lNormal(-1,sigma), lNormal(0,2*sigma) ; 
-       lNormal(1,sigma),  lNormal(0,2*sigma) };
+       lNormal(0,2*sigma),  lNormal(1, sigma) };
 
 % matrix Law
 mL= matrixLaw(A,E,Laws,n);
 
 % Signal synthesis
-X =mL.rv();
+[X, HMC ] =mL.hrv();
+% X : signal 
+% HMC : hidden markov chain
 
-plot(X)
+
+plot(X, 'k-' ); hold on; plot(HMC, 'r--' ); hold off
